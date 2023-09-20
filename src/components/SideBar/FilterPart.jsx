4907db56f827061da8ed4be8577e7045b9575b89
Tanjil Hossain <tanjilhossain845.com>
2023-09-20 09:35:57 +0600

    added radio button handler

--- src/components/SideBar/FilterPart.jsx
+++ src/components/SideBar/FilterPart.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const FilterPart = () => {
+  const [radio, setRadio] = useState("all");
+  const handelChangeRadioButton = (name) => {
+    setRadio(name);
+  };
   return (
     <div className="sidebar-content">
       <h4>Filter</h4>
@@ -9,14 +15,22 @@ const FilterPart = () => {
             type="radio"
             name="filter"
             id="lws-all"
-            checked
+            checked={radio == "all"}
             className="radio"
+            onChange={() => handelChangeRadioButton("all")}
           />
-          <label for="lws-all">All</label>
+          <label htmlFor="lws-all">All</label>
         </div>
         <div>
-          <input type="radio" name="filter" id="lws-saved" className="radio" />
-          <label for="lws-saved">Saved</label>
+          <input
+            type="radio"
+            name="filter"
+            id="lws-saved"
+            className="radio"
+            checked={radio == "saved"}
+            onChange={() => handelChangeRadioButton("saved")}
+          />
+          <label htmlFor="lws-saved">Saved</label>
         </div>
       </div>
     </div>
